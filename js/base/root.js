59892ae555c0b6e996b0afaabf415712439c0a3c
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2013-12-17 16:37:04 +0530

    moved render page to root

--- js/base/root.js
+++ js/base/root.js
@@ -1,4 +1,39 @@
-define(['base/view', 'base/model', 'widgets/header'], function(BaseView, BaseModel, Header) {
+define(['base/view', 'base/model', 'widgets/header', 'base/app'], function(BaseView, BaseModel, Header, baseApp) {
+
+    var currentPageView;
+
+    var activeApp;
+
+
+    var renderPage = function(appId, pageId, params){
+        if(currentPageView){
+            console.log('currentPageView removed ', new Date().toLocaleTimeString());
+            currentPageView.remove();
+        }
+
+        require(['apps/' + appId + '/pages/'+pageId], function(Page){
+            var view = new Page.View({
+                model: new Page.Model(params)
+            });
+            var el = $(baseApp.appBody);
+            el.empty();
+            el.html(view.el);
+            view.render();
+            currentPageView = view;
+        })
+    }
+
+    var renderApp = function(attr){
+        require(['apps/' + attr.appId], function() {
+            require(['apps/' + attr.appId + '/app'], function(currentApp) {
+                var pageId = attr.pageId || currentApp.defaultPage;
+                activeApp = currentApp;
+                currentApp.setupApp(function(){
+                    renderPage(attr.appId,pageId, attr);
+                })
+            });
+        });
+    }
 
     var RootView = BaseView.extend({
         postRender: function() {
@@ -11,14 +46,17 @@ define(['base/view', 'base/model', 'widgets/header'], function(BaseView, BaseMod
         changeHandler: function(changes) {
             var attr = this.model.toJSON();
             if (changes.appId) {
-                require(['apps/' + attr.appId], function() {
-                    require(['apps/' + attr.appId + '/app'], function(app) {
-                        app.renderPage(attr.pageId, attr);
+                if(activeApp){
+                    activeApp.tearApp(function(){
+                        renderApp(attr);
                     });
-                });
+                }else{
+                    renderApp(attr);
+                }
+
             }else if (changes.pageId) {
                 require(['apps/' + attr.appId + '/app'], function(app) {
-                    app.renderPage(attr.pageId, attr);
+                    renderPage(attr.appId,attr.pageId, attr);
                 });
             }
         }
