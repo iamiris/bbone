628f20dee18597f9703fed9d078e1caa9349f6e4
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2014-01-14 19:13:58 +0530

    chart initial commit

---
+++ js/widgets/chart.js
@@ -0,0 +1,15 @@
+define([
+    'base/app',
+    'base'
+    ],
+    function(app, Base) {
+        'use strict';
+
+        //TODO: Line Bar Stacked Bar Staked Area Bar with negative stack Dual Axis with line
+
+        return {
+            View: Base.View,
+            Model: Base.Model
+        };
+
+    });
